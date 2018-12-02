77bab0b4fd1642e7dd5e77e042f32070fbcc8030
Sandeep Jadoonanan <jsanweb@gmail.com>
2018-12-02 04:15:11 -0500

    Wire up testing with Jest and react-testing-library

---
+++ src/__tests__/simple.test.js
@@ -0,0 +1,5 @@
+describe('Simple Test', () => {
+    it('Should add properly', () => {
+        expect(1 + 1).toEqual(2);
+    });
+});
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default () => (
     <div>
-        <Link href='/'><a>Home</a></Link>
-        <Link href='/about'><a>About</a></Link>
+        <Link href='/'><a data-testid='link-home'>Home</a></Link>
+        <Link href='/about'><a data-testid='link-about'>About</a></Link>
         {/* If this route is reloaded explicitly, then a 404 will be thrown. */}
         <Link href='/other/resource?id=1' as='/resource/1'><a>Resource</a></Link>
         <style jsx>{`
